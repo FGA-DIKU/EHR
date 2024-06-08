027e5c691a597093c9009edb7c65e616ad4d292e
Kiril Klein <kikl@di.ku.dk>
2024-06-08 09:05:11 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='corebehrt',
+      version='0.9',
+      description='',
+      long_description='',
+      author='Mikkel Odgaard, Kiril Klein',
+      author_email='kikl@di.ku.dk',
+      url="https://github.com/kirilklein/ehr2vec.git",
+      packages=['corebehrt'],
+     )
