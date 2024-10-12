52be7d6314f3ad08240774c9eefd34fa17927768
Kiril Klein <kikl@di.ku.dk>
2024-10-12 14:06:36 +0200

    simplified split_paths + testing

--- corebehrt/functional/files_and_folders.py
+++ corebehrt/functional/files_and_folders.py
@@ -1,14 +1,7 @@
-import os
+from pathlib import Path
+from typing import List
 
 
-def split_path(path_str: str) -> list:
+def split_path(path_str: str) -> List[str]:
     """Split path into its components."""
-    directories = []
-    while path_str:
-        path_str, directory = os.path.split(path_str)
-        # If we've reached the root directory
-        if directory:
-            directories.append(directory)
-        elif path_str:
-            break
-    return directories[::-1]  # Reverse the list to get original order
+    return list(Path(path_str).parts)
