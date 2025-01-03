6bdad3308ee23a5e80f5b78461482d1b62e38f49
Kiril Klein <kikl@di.ku.dk>
2025-01-03 16:35:52 +0100

    remove unused args

--- corebehrt/classes/excluder.py
+++ corebehrt/classes/excluder.py
@@ -10,13 +10,9 @@ class Excluder:
         self,
         min_age: int = -1,
         max_age: int = 120,
-        min_len: int = 0,
-        background_length: int = 0,
     ):
         self.min_age = min_age
         self.max_age = max_age
-        self.min_len = min_len
-        self.background_length = background_length
 
     def exclude_incorrect_events(self, df: dd.DataFrame) -> dd.DataFrame:
         """Exclude events with incorrect ages (outside defined range)"""
