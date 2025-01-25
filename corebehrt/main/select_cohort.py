6acc62ff77a7a0486a0fa5879daf4e0f10995a56
Kiril Klein <kikl@di.ku.dk>
2025-01-25 19:14:21 +0100

    Initial version.
    Refactoring and testing needed!

--- corebehrt/main/select_cohort.py
+++ corebehrt/main/select_cohort.py
@@ -2,21 +2,27 @@
 Input: Formatted Data
 - Load patients_info, exposures, outcomes
 - Exclude by sex, age, dead before index date, outcome before index date
-- Split data
 """
 
 import logging
+from datetime import datetime
 from os.path import join
-from typing import Tuple
+from typing import List, Tuple
 
 import numpy as np
+import pandas as pd
 import torch
 
-from corebehrt.classes.loader import FormattedDataLoader
+from corebehrt.classes.outcomes import OutcomeHandler
 from corebehrt.common.config import load_config
 from corebehrt.common.setup import DirectoryPreparer, get_args
+from corebehrt.functional.filter import filter_by_column_rule
+from corebehrt.functional.utils import select_first_event
 
 CONFIG_PATH = "./corebehrt/configs/select_cohort.yaml"
+
+PID_COL = "PID"
+TIMESTAMP_COL = "TIMESTAMP"
 
 
 def main_select_cohort(config_path):
@@ -40,11 +46,8 @@ def main_select_cohort(config_path):
     pids, index_dates = select_cohort(cfg)
     logger.info("Finished cohort selection")
 
-    logger.info("Split pids")
-    splits = split_pids_into_train_val_test(pids, **cfg.split_ratios)
-    logger.info("Save splits")
-    torch.save(splits, join(cfg.paths.data, "splits.pt"))
-    torch.save(index_dates, join(cfg.paths.data, "index_dates.pt"))
+    torch.save(pids, join(cfg.paths.cohort, "pids.pt"))
+    torch.save(index_dates, join(cfg.paths.cohort, "index_dates.pt"))
 
 
 def split_pids_into_train_val_test(
@@ -102,21 +105,146 @@ def select_cohort(cfg) -> Tuple[list, dict]:
     Creates features and saves them to disk.
     Returns a list of lists of pids for each batch
     """
-    concepts, patients_info = FormattedDataLoader(
-        cfg.paths.data,
-        cfg.loader.concept_types,
-        include_values=(getattr(cfg.loader, "include_values", [])),
-    ).load()
+    patients_info, outcomes, exposures, initial_pids = load_data(cfg)
 
-    #########################################################
-    # Load outcomes/ exposures and index events/ set index dates
-    # sex exclusion: via patients_info select sex
-    # age exclusion: calculate age at index date
-    # dead before index date: via patients_info select dead
-    # outcome before index date: via patients_info select outcome
-    # exposure selection: e.g. pregnant only
-    # save remaining pids and index dates
-    #########################################################
+    if initial_pids:
+        patients_info = select_pids(patients_info, initial_pids)
+
+    if cfg.selection.get("exposed_only", False):
+        patients_info = select_pids(patients_info, exposures[PID_COL].unique())
+
+    # Filter by sex, hospital, etc.
+    if cfg.selection.get("categories", False):
+        patients_info = filter_by_column_rules(patients_info, cfg)
+
+    index_dates = get_index_dates(patients_info, cfg.index_date, exposures)
+
+    patients_info = filter_by_age(
+        patients_info,
+        index_dates,
+        cfg.selection.get("age_min", None),
+        cfg.selection.get("age_max", None),
+    )
+
+    if cfg.selection.get("dead_before_index_date", False):
+        patients_info = patients_info[patients_info["DEATHDATE"] > index_dates]
+
+    if cfg.selection.get("outcome_before_index_date", False):
+        # align outcomes with patients_info
+        outcomes = pd.Series(index=outcomes[PID_COL], data=outcomes["TIMESTAMP"])
+        outcomes = outcomes.reindex(patients_info[PID_COL])
+        outcomes = outcomes[outcomes < index_dates]
+        patients_to_exclude = set(outcomes.index)
+        patients_info = patients_info[~patients_info[PID_COL].isin(patients_to_exclude)]
+
+    final_pids = patients_info[PID_COL].unique().tolist()
+    return final_pids, index_dates
+
+
+def filter_by_age(
+    patients_info: pd.DataFrame,
+    index_dates: pd.Series,
+    min_age: int = None,
+    max_age: int = None,
+) -> pd.DataFrame:
+    if min_age:
+        patients_info = patients_info[
+            (index_dates - patients_info["BIRTHDATE"]) >= min_age
+        ]
+    if max_age:
+        patients_info = patients_info[
+            (index_dates - patients_info["BIRTHDATE"]) <= max_age
+        ]
+    return patients_info
+
+
+def filter_by_column_rules(patients_info: pd.DataFrame, cfg: dict) -> pd.DataFrame:
+    for col, rule in cfg.selection.categories.items():
+        patients_info = filter_by_column_rule(
+            patients_info,
+            column=col,
+            include_values=rule.get("include"),
+            exclude_values=rule.get("exclude"),
+        )
+    return patients_info
+
+
+def select_pids(patients_info: pd.DataFrame, pids: List[str]) -> pd.DataFrame:
+    pids = set(pids)
+    return patients_info[patients_info[PID_COL].isin(pids)]
+
+
+def load_data(cfg):
+    patients_info = read_table(
+        cfg.paths.patients_info, parse_dates=["BIRTHDATE", "DEATHDATE"]
+    )
+    outcomes = read_table(cfg.paths.outcome, parse_dates=[TIMESTAMP_COL])
+    if cfg.paths.get("exposure", False):
+        exposures = read_table(cfg.paths.exposure, parse_dates=[TIMESTAMP_COL])
+    else:
+        exposures = outcomes
+    exposures = select_first_event(exposures, PID_COL, TIMESTAMP_COL)
+    initial_pids = (
+        set(torch.load(cfg.paths.initial_pids))
+        if cfg.paths.get("initial_pids", False)
+        else set()
+    )
+    return patients_info, outcomes, exposures, initial_pids
+
+
+def get_index_dates(
+    patients_info: pd.DataFrame, index_date: dict, exposures: pd.DataFrame = None
+) -> pd.Series:
+    pids = set(patients_info[PID_COL].unique())
+    if index_date["mode"] == "absolute":
+        index_dates = create_timestamp_series(pids, index_date["absolute"])
+    elif index_date["mode"] == "relative":
+        n_hours_from_exposure = index_date["relative"]["n_hours_from_exposure"]
+        exposed_index_timestamps = get_index_timestamps_for_exposed(
+            pids, n_hours_from_exposure, exposures
+        )
+        index_dates = OutcomeHandler.draw_index_dates_for_unexposed(
+            exposed_index_timestamps, pids
+        )
+    # align index dates with patients_info
+    index_dates = index_dates.reindex(patients_info[PID_COL])
+    return index_dates
+
+
+def get_index_timestamps_for_exposed(
+    pids: set, n_hours_from_exposure: int, exposures: pd.DataFrame
+):
+    # Convert hours to timedelta
+    hours_delta = pd.Timedelta(hours=n_hours_from_exposure)
+    exposures = exposures[exposures[PID_COL].isin(pids)]
+    index_dates = exposures["TIMESTAMP"] + hours_delta
+    return index_dates
+
+
+def create_timestamp_series(pids: set, timestamp: dict) -> pd.Series:
+    """
+    Create a Series with PID index and timestamp values.
+
+    Args:
+        pids: Set of patient IDs
+        timestamp: Single timestamp value for all entries
+
+    Returns:
+        pandas Series with PID index and timestamp values
+    """
+    timestamp = datetime(**timestamp)
+    return pd.Series(
+        data=timestamp, index=pd.Index(list(pids), name="PID"), name="TIMESTAMP"
+    )
+
+
+def read_table(path: str, parse_dates: list[str] = []) -> pd.DataFrame:
+    if path.endswith(".csv"):
+        return pd.read_csv(path, parse_dates=parse_dates)
+    elif path.endswith(".parquet"):
+        return pd.read_parquet(path)
+    else:
+        raise ValueError(f"Unsupported file type: {path}")
 
 
 if __name__ == "__main__":
