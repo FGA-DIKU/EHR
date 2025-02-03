3f5f74818d245a8dd03a706b5542e32f94206887
Kiril Klein <kikl@di.ku.dk>
2025-02-03 06:51:05 +0100

    Introduce bucketed dynamic padding

--- corebehrt/common/constants.py
+++ corebehrt/common/constants.py
@@ -1,11 +1,19 @@
+
+### Columns ###
 PID_COL = "PID"
 TIMESTAMP_COL = "TIMESTAMP"
 BIRTHDATE_COL = "BIRTHDATE"
 DEATHDATE_COL = "DEATHDATE"
 ABSPOS_COL = "abspos"
 
+### File Names ###
 CONCEPT_FORMAT = "concept.*"
 PATIENTS_INFO_FORMAT = "patients_info.*"
 
+### File Extensions ###
 CSV_EXT = ".csv"
 PARQUET_EXT = ".parquet"
+
+#### Trainer ### 
+DEFAULT_BUCKETS = [64, 128, 256, 512, 1024, 2048, 4096, 8192]
+DEFAULT_NUM_WORKERS = 8
