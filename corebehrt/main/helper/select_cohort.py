e3e27ad78c59e9dba4ff6c6f895d6777049caf50
Kiril Klein <kikl@di.ku.dk>
2025-02-06 09:31:50 +0100

    Improve cohort_selection functions
    * Make more modular functions for patient filter
    * add additional functions e.g. exclude pids
    * improve unittest for patient_filter

--- corebehrt/main/helper/select_cohort.py
+++ corebehrt/main/helper/select_cohort.py
@@ -7,9 +7,11 @@ from corebehrt.constants.data import PID_COL, TIMESTAMP_COL
 from corebehrt.functional.preparation.filter import select_first_event
 from corebehrt.modules.cohort_handling.index_dates import IndexDateHandler
 from corebehrt.modules.cohort_handling.patient_filter import (
-    apply_exclusion_filters,
+    exclude_pids_from_df,
     filter_by_age,
     filter_by_categories,
+    filter_by_death,
+    filter_by_prior_outcomes,
     filter_df_by_pids,
 )
 from corebehrt.modules.features.loader import ConceptLoader
@@ -27,7 +29,7 @@ def select_cohort(cfg, logger) -> Tuple[List[str], pd.Series]:
         Tuple of final patient IDs and their index dates
     """
     logger.info("Loading data")
-    patients_info, outcomes, exposures, initial_pids = load_data(cfg)
+    patients_info, outcomes, exposures, initial_pids, exclude_pids = load_data(cfg)
     patients_info = patients_info.drop_duplicates(subset=PID_COL, keep="first")
     logger.info("N patients_info: %d", len(patients_info))
     logger.info("Patients in initial_pids: %d", len(initial_pids))
@@ -37,8 +39,14 @@ def select_cohort(cfg, logger) -> Tuple[List[str], pd.Series]:
         patients_info = filter_df_by_pids(patients_info, initial_pids)
         log_patient_num(logger, patients_info)
 
+    if exclude_pids:
+        logger.info("Filtering by exclude_pids")
+        patients_info = exclude_pids_from_df(patients_info, exclude_pids)
+        log_patient_num(logger, patients_info)
+
     if cfg.selection.get("exposed_only", False):
         logger.info("Filtering by exposures")
+
         patients_info = filter_df_by_pids(patients_info, exposures[PID_COL])
         log_patient_num(logger, patients_info)
 
@@ -70,12 +78,13 @@ def select_cohort(cfg, logger) -> Tuple[List[str], pd.Series]:
         log_patient_num(logger, patients_info)
 
     logger.info("Applying additional exclusion filters")
-    patients_info = apply_exclusion_filters(
-        patients_info,
-        outcomes,
-        outcome_before_index_date=cfg.selection.get("exclude_prior_outcomes", False),
-    )
+    patients_info = filter_by_death(patients_info)
     log_patient_num(logger, patients_info)
+
+    if cfg.selection.get("exclude_prior_outcomes", False):
+        logger.info("Filtering by prior outcomes")
+        patients_info = filter_by_prior_outcomes(patients_info, outcomes)
+        log_patient_num(logger, patients_info)
     return patients_info[PID_COL].unique().tolist(), index_dates
 
 
@@ -83,7 +92,9 @@ def log_patient_num(logger, patients_info):
     logger.info(f"Patient number: {len(patients_info[PID_COL].unique())}")
 
 
-def load_data(cfg):
+def load_data(
+    cfg,
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, List[str], List[str]]:
     """Load patient, outcomes, and exposures data."""
     patients_info = ConceptLoader.read_file(cfg.paths.patients_info)
     outcomes = ConceptLoader.read_file(cfg.paths.outcome)
@@ -102,4 +113,10 @@ def load_data(cfg):
         else []
     )
 
-    return patients_info, outcomes, exposures, initial_pids
+    exclude_pids = (
+        torch.load(cfg.paths.exclude_pids)
+        if cfg.paths.get("exclude_pids", False)
+        else []
+    )
+
+    return patients_info, outcomes, exposures, initial_pids, exclude_pids
