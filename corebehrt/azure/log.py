d2af636285c914d9cac4035943bd394c6ab0b7e4
StephanLorenzen <stephan.lorenzen@gmail.com>
2025-02-10 23:34:35 +0100

    Comments

--- corebehrt/azure/log.py
+++ corebehrt/azure/log.py
@@ -3,6 +3,7 @@ from corebehrt.azure.util import is_azure_available
 MLFLOW_AVAILABLE = False
 
 try:
+    # Try to import mlflow and set availability flag
     import mlflow
 
     MLFLOW_AVAILABLE = True
@@ -10,26 +11,50 @@ except:
     pass
 
 
-def is_mlflow_available():
+def is_mlflow_available() -> bool:
+    """
+    Checks if azure and mlflow modules are available.
+    """
     global MLFLOW_AVAILABLE
     return is_azure_available() and MLFLOW_AVAILABLE
 
 
 def start_run():
+    """
+    Starts an mlflow run. Used in the Azure wrapper and should
+    not in general be used elsewhere.
+    """
     if is_mlflow_available():
         mlflow.start_run()
 
 
 def end_run():
+    """
+    Ends an mlflow run. Used in the Azure wrapper and should
+    not in general be used elsewhere.
+    """
     if is_mlflow_available():
         mlflow.end_run()
 
 
 def autolog(*args, **kwargs):
+    """
+    Enables mlflow autologging (if mlflow is available)
+    """
     if is_mlflow_available():
         mlflow.autolog(*args, **kwargs)
 
 
 def log_metric(*args, **kwargs):
+    """
+    Logs a metric to the job (if mlflow is available).
+    Parameters are the same as for mlflow.log_metric.
+
+    Important parameters:
+
+    :param name: Name of the metric.
+    :param value: Value of the metric.
+    :param step: Step for the metric (used for plotting graphs).
+    """
     if is_mlflow_available():
         mlflow.log_metric(*args, **kwargs)
