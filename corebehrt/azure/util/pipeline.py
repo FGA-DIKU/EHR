3ee18b7a9bf5064e8c4622ed0b432fb6d83fd02c
StephanLorenzen <stephan.lorenzen@gmail.com>
2025-03-03 13:22:47 +0100

    Added pipeline create and run.

--- corebehrt/azure/util/pipeline.py
+++ corebehrt/azure/util/pipeline.py
@@ -1,4 +1,6 @@
+import importlib
 from corebehrt.azure.util import check_azure, job
+from corebehrt.azure.util.config import map_azure_path
 
 
 def create_component(
@@ -17,3 +19,37 @@ def create_component(
     }
 
     return job.create(name, config, compute, register_output, log_system_metrics)
+
+
+def create(
+    name: str,
+    configs: dict,
+    computes: dict,
+    register_output: dict,
+    log_system_metrics: bool,
+) -> "command":  # noqa: F821
+    check_azure()
+
+    from azure.ai.ml import Input
+
+    # Load pipeline module
+    pipeline_module = importlib.import_module(f"corebehrt.azure.pipelines.{name}")
+
+    # Create pipeline command
+    pipeline = pipeline_module.create(
+        configs, computes, register_output, log_system_metrics
+    )
+
+    # Prepare pipeline inputs
+    inputs = dict()
+    for inp_key, inp_cfg in pipeline_module.INPUTS.items():
+        job_name = inp_cfg["config"]
+        inp_path = map_azure_path(configs[job_name]["paths"][inp_key])
+
+        inputs[inp_key] = Input(path=inp_path, type=inp_cfg.get("type"))
+
+    return pipeline(**inputs)
+
+
+def run(pipeline: "command", experiment: str) -> None:  # noqa: F821
+    return job.run(pipeline, experiment)
