a36a03d52b077cb6604b6d25798f4e0558b0ca67
StephanLorenzen <stephan.lorenzen@gmail.com>
2025-03-10 14:57:00 +0100

    Updated pipeline

--- corebehrt/azure/pipelines/E2E.py
+++ corebehrt/azure/pipelines/E2E.py
@@ -25,11 +25,21 @@ def create(
             features=create_data.outputs.features,
         )
 
-        pretrain = create_component(
-            "pretrain", configs, computes, register_output, log_system_metrics
+        prepare_pretrain = create_component(
+            "prepare_training_data",
+            configs,
+            computes,
+            register_output,
+            log_system_metrics,
         )(
             features=create_data.outputs.features,
             tokenized=create_data.outputs.tokenized,
+        )
+
+        pretrain = create_component(
+            "pretrain", configs, computes, register_output, log_system_metrics
+        )(
+            prepared_data=prepare_pretrain.outputs.prepared_data,
         )
 
         return {}
