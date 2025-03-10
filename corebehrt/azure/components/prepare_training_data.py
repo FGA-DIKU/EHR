598d665f63508860c1efcb3afdd32ac20f2e2082
StephanLorenzen <stephan.lorenzen@gmail.com>
2025-03-10 14:54:47 +0100

    Missing job name

--- corebehrt/azure/components/prepare_training_data.py
+++ corebehrt/azure/components/prepare_training_data.py
@@ -16,4 +16,9 @@ OUTPUTS = {"prepared_data": {"type": "uri_folder"}}
 if __name__ == "__main__":
     from corebehrt.main import prepare_training_data
 
-    job.run_main(prepare_training_data.main_prepare_data, INPUTS, OUTPUTS)
+    job.run_main(
+        "prepare_training_data",
+        prepare_training_data.main_prepare_data,
+        INPUTS,
+        OUTPUTS,
+    )
