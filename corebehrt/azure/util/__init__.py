a71b20687d671212c20c71562191b3ec5165a7ce
StephanLorenzen <stephan.lorenzen@gmail.com>
2025-03-17 12:55:12 +0100

    Small fixes

--- corebehrt/azure/util/__init__.py
+++ corebehrt/azure/util/__init__.py
@@ -1,4 +1,4 @@
 from corebehrt.azure.util.azure import is_azure_available, check_azure, ml_client
-from corebehrt.azure.util import job, pipeline
+from corebehrt.azure.util import job, pipeline, test
 
-__all__ = [is_azure_available, check_azure, ml_client, job, pipeline]
+__all__ = [is_azure_available, check_azure, ml_client, job, pipeline, test]
