c6b4aaac180b6139cfe0794e9a4b7a8f3d2a1963
Kiril Klein <kikl@di.ku.dk>
2025-03-18 10:25:37 +0100

    add mapping functionality: (#193)
    * modify tokenizer
    * add unittests
    * add optional inputs to create_data
    * update config

--- corebehrt/azure/components/create_data.py
+++ corebehrt/azure/components/create_data.py
@@ -1,6 +1,10 @@
 from corebehrt.azure import util
 
-INPUTS = {"data": {"type": "uri_folder"}}
+INPUTS = {
+    "data": {"type": "uri_folder"},
+    "vocabulary": {"type": "uri_folder", "optional": True},
+    "code_mapping": {"type": "uri_file", "optional": True},
+}
 OUTPUTS = {
     "tokenized": {"type": "uri_folder"},
     "features": {"type": "uri_folder"},
